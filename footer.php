b1f92426b65bf0529189500babebc049454ba5bd
AmitSakhare <66629728+AmitSakhare@users.noreply.github.com>
2020-10-25 23:21:31 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,30 +9,8 @@
 						<a href="about.php" class="btn btn-primary read-m">Read More</a>
 					</div>
 				</div>
-				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
-
-					<div class="tech-btm">
-						<h3>Popular Posts</h3>
-						<?php getpopularposts("page_hits"); ?>
-					</div>
-				</div>
-				<!-- subscribe -->
-				<div class="col-lg-4 subscribe-main footer-grid-agileits-w3ls text-left">
-					<h2>Signup to our newsletter</h2>
-					<div class="subscribe-main text-left">
-							<div class="subscribe-form">
-									<form action="#" method="post" class="subscribe_form">
-										<input class="form-control" type="email" placeholder="Enter your email..." required="">
-										<button type="submit" class="btn btn-primary submit">Submit</button>
-									</form>
-									<div class="clearfix"> </div>
-						   </div>
-						<p>We respect your privacy.No spam ever!</p>
-					</div>
-					<!-- //subscribe -->
-				</div>
-			</div>
-			<!-- footer -->
+<!-- subscribe -->
+				<!-- footer -->
 			<div class="footer-cpy text-center">
 				<div class="footer-social">
 					<div class="copyrighttop">
@@ -65,13 +43,7 @@
 
 					</div>
 				</div>
-				<div class="w3layouts-agile-copyrightbottom">
-					<p> Responsive Blog Site <?php $current=date("Y"); print_r($current);?> | Brought To You by
-						<a href="http://code-projects.org/">Code-Projects</a>
-					</p>
-
-				</div>
-			</div>
+				
 			<!-- //footer -->
 		</div>
 	</footer>
